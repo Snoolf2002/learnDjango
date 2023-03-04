557ce7bdbf07e2cdbbcf91b7c05be4f9ed8db6e3
Snoolf001 <abdurahmonovjaloliddin2002@gmail.com>
2023-03-04 09:08:12 +0500

    Some changes

--- core/urls.py
+++ core/urls.py
@@ -1,27 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpRequest, HttpResponse, JsonResponse
+from base.views import index, forJsonResponse, forPost
 
-def index(request:HttpRequest):
-        a = request.GET.get('a')
-        b = request.GET.get('b')
-
-        if a!=None and b!=None:    
-            return HttpResponse(f"{a}+{b}={int(a)+int(b)}")
-        
-        return HttpResponse("Hey you must give two numbers")
-
-def forJsonResponse(request:HttpRequest):
-    a = request.GET.get('a')
-    b = request.GET.get('b')
-
-    if a!=None and b!=None:  
-        return JsonResponse({
-            'result': f"{a}+{b}={int(a)+int(b)}"
-        })
-    return HttpResponse("Hey you must give two numbers")
 
 urlpatterns = [
     path('', index),
-    path('salom/', forJsonResponse)
+    path('salom/', forJsonResponse),
+    path('check/', forPost)
 ]
